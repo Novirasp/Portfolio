c148452c8ae5ee5494b8c77552d36581b2d3fb79
Novirasp <noviraprajna3@gmail.com>
2026-01-06 00:33:18 +0700

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 export const timelineData = [
     {
-        title: 'Moderator for Adjunct Professor',
+        title: 'Moderator for Adjunct Proffesor',
         institution: 'Universitas Brawijaya',
         period: 'October 2025',
         image: 'assets/Screenshot 2025-11-18 075133.png',
@@ -33,6 +33,12 @@ export const timelineData = [
         image: 'assets/Screenshot_2023-08-28-08-28-43-844-edit_com.android.chrome.jpg',
         imageSize: "w-60 h-60",
         imageMargin: "-ml-14",
+
+        source: {
+        text: 'Source: MemoX.co.id, 2023',
+        link: 'https://memox.co.id/bem-feb-ub-gelar-workshop-pengembangan-bisnis-umkm-di-era-digitalisasi-5-0-bersama-alfamart/#:~:text=MEMOX.CO.ID%20%2D%20Badan%20Eksekutif%20Mahasiswa%20(BEM)%20Fakultas,Bisnis%20Universitas%20Brawijaya%20(UB)%20menggelar%20workshop%20sehubungan'
+        },
+
         points: [
             'Awarded "Staff of the Month" in June and July 2023.',
             'Project Leader for the MSME marketing and financial digitalization across Malang City',
@@ -45,13 +51,13 @@ export const timelineData = [
 export const projectsData = [
     {
         id: 1,
-        title: 'Contribution of Information Technology and Communication to Poverty in Indonesia (Continuous Paper)',
+        title: 'Kontribusi Teknologi Informasi dan Komunikasi terhadap Kemiskinan di Indonesia',
         short_desc: 'A quantitative studies about ICT in Indonesia.',
         objective: 'Conducted a quantitative study analyzing how Information and Communication Technology (ICT) contributes to poverty reduction across Indonesia’s provinces from 2012–2024.',
         role: 'Lead Researcher, responsible for data collection from multiple sources, cleaning and processing data, performing econometric analysis using panel regression techniques, and interpreting results to draw conclusions about the impact of ICT on poverty levels.',
         tools: 'Data Panel Regression, Interation model, Stata, and Python',
         impact: 'The findings indicate that ICT contributes to poverty reduction, although the magnitude varies across provinces due to differing regional characteristics. Interestingly, the effectiveness of ICT in reducing poverty is higher in provinces with lower economic performance, suggesting that ICT adoption may generate greater marginal benefits in less developed regions.',
-        link: '#'
+        link: 'https://drive.google.com/file/d/1bVKd4x1Q2I1Ca-2zQptcycxzbDxgtISV/view?usp=sharing'
     },
     {
         id: 2,
@@ -79,4 +85,3 @@ export const skillsData = {
     labels: ['Microsoft', 'Python', 'Stata', 'Power BI'],
     values: [95, 85, 75, 70, 90, 80]
 };
-
